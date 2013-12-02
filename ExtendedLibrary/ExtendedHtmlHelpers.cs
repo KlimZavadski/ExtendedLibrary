7b89c8a88e74eadc2f30a3520d8dc858558acef6
Klim Zavadski <Klim_911@tut.by>
2013-12-02 18:21:12 +0300

    Added SSL support.

--- ExtendedLibrary/ExtendedHtmlHelpers.cs
+++ ExtendedLibrary/ExtendedHtmlHelpers.cs
@@ -12,27 +12,29 @@ namespace ExtendedLibrary
     public static class ExtendedWebHelpers
     {
         /// <summary>
-        /// Get response from site.
+        /// Get response from site by https protocol (used SSL).
         /// </summary>
-        /// <param name="url">Url for response.</param>
-        /// <param name="host">Host like as "kinopoisk.ru"</param>
+        /// <param name="url">Url from get redirect.</param>
+        /// <param name="login">User's login</param>
+        /// <param name="password">User's password</param>
         /// <returns></returns>
-        public static WebResponse GetResponse(String url, String host)
+        public static WebResponse GetResponse(String url, String login, String password)
         {
             try
             {
                 var request = (HttpWebRequest)WebRequest.Create(url);
                 request.AllowAutoRedirect = false;
                 request.Method = "GET";
-                request.Host = "www." + host;
                 request.UserAgent = "User-Agent=Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.1.7) Gecko/20091221 Firefox/3.5.7 (.NET CLR 3.5.30729)";
                 request.Headers.Add(HttpRequestHeader.AcceptLanguage, @"ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
                 request.Headers.Add(HttpRequestHeader.AcceptEncoding, @"gzip, deflate");
                 request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
                 request.ContentType = "application/x-www-form-urlencoded";
                 request.ProtocolVersion = HttpVersion.Version11;
-                request.Referer = "http://" + host;
+                request.KeepAlive = false;
+                request.PreAuthenticate = true;
                 request.CookieContainer = new CookieContainer();
+                request.Credentials = new NetworkCredential(login, password);
 
                 return request.GetResponse();
             }
@@ -43,14 +45,15 @@ namespace ExtendedLibrary
         }
 
         /// <summary>
-        /// Get redirect url.
+        /// Get redirect url by https protocol (used SSL).
         /// </summary>
         /// <param name="url">Url from get redirect.</param>
-        /// <param name="host">Host like as "kinopoisk.ru"</param>
+        /// <param name="login">User's login</param>
+        /// <param name="password">User's password</param>
         /// <returns></returns>
-        public static String GetRedirectUrl(String url, String host)
+        public static String GetRedirectUrl(String url, String login, String password)
         {
-            var responce = (HttpWebResponse)GetResponse(url, host);
+            var responce = (HttpWebResponse)GetResponse(url, login, password);
             if (responce != null)
             {
                 return responce.ResponseUri.ToString();
@@ -59,14 +62,25 @@ namespace ExtendedLibrary
         }
 
         /// <summary>
-        /// Get response from site as HTML content.
+        /// Get redirect url by http protocol.
         /// </summary>
-        /// <param name="url">Url for response.</param>
-        /// <param name="host">Host like as "kinopoisk.ru"</param>
+        /// <param name="url">Url from get redirect.</param>
         /// <returns></returns>
-        public static String GetResponseHtml(String url, String host)
+        public static String GetRedirectUrl(String url)
         {
-            var response = (HttpWebResponse)GetResponse(url, host);
+            return GetRedirectUrl(url, String.Empty, String.Empty);
+        }
+
+        /// <summary>
+        /// Get response from site as HTML content by https protocol (used SSL).
+        /// </summary>
+        /// <param name="url">Url from get redirect.</param>
+        /// <param name="login">User's login</param>
+        /// <param name="password">User's password</param>
+        /// <returns></returns>
+        public static String GetResponseHtml(String url, String login, String password)
+        {
+            var response = (HttpWebResponse)GetResponse(url, login, password);
             if (response != null)
             {
                 var encoding = Encoding.GetEncoding(response.CharacterSet);
@@ -76,6 +90,16 @@ namespace ExtendedLibrary
                 }
             }
             return String.Empty;
+        }
+
+        /// <summary>
+        /// Get response from site as HTML content by http protocol.
+        /// </summary>
+        /// <param name="url">Url for response.</param>
+        /// <returns></returns>
+        public static String GetResponseHtml(String url)
+        {
+            return GetResponseHtml(url, String.Empty, String.Empty);
         }
     }
 }
